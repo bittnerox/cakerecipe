f472cf5f2850c98596583c9aa847c075d77e7b24
bittner williams <bittwill12@gmail.com>
2022-06-23 22:24:43 -0700

    commit

--- first.js
+++ first.js
@@ -1,4 +1,2 @@
 alert('hello and welcome to the website!');
-alert('welcome!');
-alert('what is your name!');
-document.getElementById("demo").innerHTML = "Hello Javascript";
+prompt('my name is williams what is yours') ('enter your name');
